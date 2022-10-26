5b2cdf6a5f346e1085f36a6c8188937338262e23
faizan3165 <faizanmehmood3165@gmail.com>
2022-10-26 18:27:46 +0500

    add quote total feature

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,8 @@
 class Quote < ApplicationRecord
     has_many :line_item_dates, dependent: :destroy
 
+    has_many :line_items, through: :line_item_dates
+
     belongs_to :company
 
     validates :name, presence: true
@@ -9,4 +11,7 @@ class Quote < ApplicationRecord
 
     broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 
+    def total_price
+        line_items.sum(&:total_price)
+    end
 end
